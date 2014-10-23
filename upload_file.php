f6adf45f5250f598fd29729e52209753a6c63bd4
samspeedstar <spiritualyg@gmail.com>
2014-10-22 21:01:51 -0400

    no message

---
+++ upload_file.php
@@ -0,0 +1,19 @@
+<?php
+foreach ($_FILES["xml"]["error"] as $key => $error) {
+ /*   if (file_exists("upload/" . $_FILES["xml"]["name"]))
+      {
+      echo $_FILES["xml"]["name"] . " already exists. ";
+      }
+	  	*/
+    if ($error == UPLOAD_ERR_OK) {
+        $tmp_name = $_FILES["xml"]["tmp_name"][$key];
+        $name = $_FILES["xml"]["name"][$key];
+        move_uploaded_file($tmp_name, "uploads/$name");
+    }
+
+else
+  {
+  echo "Invalid file";
+  }
+  }
+  echo "<h4>Successfully Uploaded 3D model files</h4>";  
